a1267b6478706752b630a1e25720f68246a7cb76
Chuck Ha <cha@nvidia.com>
2023-06-23 15:22:21 -0700

    comment out azuread provider

--- lz-networking/provider.tf
+++ lz-networking/provider.tf
@@ -16,17 +16,17 @@ terraform {
   }
 }
 
-# Configure the Azure Active Directory Provider
-provider "azuread" {
+# # Configure the Azure Active Directory Provider
+# provider "azuread" {
 
-  # NOTE: Environment Variables can also be used for Service Principal authentication
-  # Terraform also supports authenticating via the Azure CLI too.
-  # See official docs for more info: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs
+#   # NOTE: Environment Variables can also be used for Service Principal authentication
+#   # Terraform also supports authenticating via the Azure CLI too.
+#   # See official docs for more info: https://registry.terraform.io/providers/hashicorp/azuread/latest/docs
 
-  # client_id     = "..."
-  # client_secret = "..."
-  # tenant_id     = "..."
-}
+#   # client_id     = "..."
+#   # client_secret = "..."
+#   # tenant_id     = "..."
+# }
 
 # terraform {
 #   # backend "azurerm" {
